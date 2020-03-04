256fd3c7fa6c53a7739b000c218c5acd5c08e0d1
sark <sarkahn@gmail.com>
2020-03-04 07:14:19 -0700

    feat: attempt 5

--- Runtime/NewBehaviourScript2.cs
+++ Runtime/NewBehaviourScript2.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello");
     }
 
     // Update is called once per frame
