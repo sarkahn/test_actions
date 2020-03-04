e778ee29942bd01db529d0436237e803b14f7ac4
sark <sarkahn@gmail.com>
2020-03-04 00:02:30 -0700

    Added ASMDef and tests

---
+++ Assets/CoolPackage/Tests/Editor/CoolTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+
+namespace CoolPackage.Tests
+{
+    [TestFixture]
+    public class CoolTests : MonoBehaviour
+    {
+        public void WHOA()
+        {
+            Assert.IsTrue(true);
+        }
+
+        public void WHOA2()
+        {
+            Assert.IsFalse(false);
+        }
+    } 
+}
