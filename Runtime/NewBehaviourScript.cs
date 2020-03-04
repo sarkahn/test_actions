c422aa1fa4625b0848c2b23249235a2a1a530a26
sark <sarkahn@gmail.com>
2020-03-04 07:22:59 -0700

    feat: did things

--- Runtime/NewBehaviourScript.cs
+++ Runtime/NewBehaviourScript.cs
@@ -9,13 +9,13 @@ namespace CoolPackage
         // Start is called before the first frame update
         void Start()
         {
-
+			
         }
 
         // Update is called once per frame
         void Update()
         {
-
+			Debug.Log("AUOENIONFA");
         }
     } 
 }
